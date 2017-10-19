c518f3fd5c7ba0f9ff93c6ccdc6e0212baccee7a
xiejia <xiejia@cnnic.cn>
2017-10-19 22:28:12 +0800

    add templates and status and edit views and urls

--- Orz/orz_app/views.py
+++ Orz/orz_app/views.py
@@ -1,3 +1,10 @@
+# -*- coding:UTF-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse(u"第一个博客")
 
 # Create your views here.
