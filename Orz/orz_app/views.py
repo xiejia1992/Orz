eafc615ea3cdcfa4cd7a80cff09c8c4497aa8495
xiejia <xiejia@cnnic.cn>
2017-10-23 09:32:06 +0800

    update view to show article caption

--- Orz/orz_app/views.py
+++ Orz/orz_app/views.py
@@ -2,9 +2,11 @@
 
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from models import *
 
 
 def index(request):
-    return render_to_response('index.html',)
+    all_article = Article.objects.all()
+    return render_to_response('index.html', {'all_article': all_article})
 
 # Create your views here.
