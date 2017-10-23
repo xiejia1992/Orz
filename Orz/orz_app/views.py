58aed343f56c13ff4cc6dbd0cbd46d64a88accdb
xiejia <xiejia@cnnic.cn>
2017-10-23 16:05:09 +0800

    add paging to index.html

--- Orz/orz_app/views.py
+++ Orz/orz_app/views.py
@@ -2,12 +2,21 @@
 
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from models import *
 
 
 def index(request):
     all_article = Article.objects.all().order_by('-publish_time')
-    return render_to_response('index.html', {'all_article': all_article})
+    paginator = Paginator(all_article, 5, 1)
+    page = request.GET.get('page')
+    try:
+        article = paginator.page(page)
+    except PageNotAnInteger:
+        article = paginator.page(1)
+    except EmptyPage:
+        article = paginator.page(paginator.num_pages)
+    return render_to_response('index.html', {'articles': article})
 
 
 def article_detail(request, id):
