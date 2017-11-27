9db3dc1a5ad812e34d0028126cc34924b36d167f
xiejia <xiejia@cnnic.cn>
2017-11-27 11:14:19 +0800

    add back-to-top

--- Orz/Orz/settings.py
+++ Orz/Orz/settings.py
@@ -81,6 +81,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'blog',
+#        'USER': 'blog',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
