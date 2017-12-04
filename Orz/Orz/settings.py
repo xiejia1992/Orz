3c50fbfb9b945fe0c9a8e20997e6163e974be40c
xiejia <xiejia@cnnic.cn>
2017-12-04 13:16:00 +0800

    change settings about databases

--- Orz/Orz/settings.py
+++ Orz/Orz/settings.py
@@ -77,20 +77,15 @@ WSGI_APPLICATION = 'Orz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-#DATABASES = {
-#    'default': {
 #        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #        'NAME': 'blog',
 #        'USER': 'root',
 #        'PASSWORD': '',
 #        'HOST': 'localhost',
 #        'PORT': '3306',
-#    }
-#}
+    }
+}
 
 
 # Password validation
